c8ad2459acc5c7e5af14d953914593030fdecf10
Guillaume Paumier <gp@guillaumepaumier.com>
2014-12-08 12:51:04 -0800

    add bash script to get Commons files

---
+++ commons.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mysql --defaults-file=/data/project/mrmetadata/.my.cnf -h commonswiki.labsdb < /data/project/mrmetadata/commons_query.txt > /data/project/mrmetadata/commons_list.txt
+sed -i.orig -e '1d' commons_list.txt
