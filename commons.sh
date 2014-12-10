e35f30703e925e99c80143024307a15e440a3d08
Guillaume Paumier <gp@guillaumepaumier.com>
2014-12-10 10:09:32 -0800

    Add Commons check to script

--- commons.sh
+++ commons.sh
@@ -2,3 +2,4 @@
 mysql --defaults-file=/data/project/mrmetadata/.my.cnf -h commonswiki.labsdb < /data/project/mrmetadata/commons_query.txt > /data/project/mrmetadata/commons_list.txt
 sed -i.orig -e '1d' commons_list.txt
 mysql --defaults-file=/data/project/mrmetadata/.my.cnf -h commonswiki.labsdb < /data/project/mrmetadata/commons_filecount_query.txt > /data/project/mrmetadata/commons_filecount.txt
+/data/project/mrmetadata/bin/python mrmetadata.py --commons
