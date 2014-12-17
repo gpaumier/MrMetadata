bd26c5724f6a79cead5a3b534991db728dae5f4d
Guillaume Paumier <gp@guillaumepaumier.com>
2014-12-17 02:30:46 +0100

    Add list of uploaders of files missing an infobox

--- commons.sh
+++ commons.sh
@@ -3,3 +3,4 @@ mysql --defaults-file=/data/project/mrmetadata/.my.cnf -h commonswiki.labsdb < /
 sed -i.orig -e '1d' commons_list.txt
 mysql --defaults-file=/data/project/mrmetadata/.my.cnf -h commonswiki.labsdb < /data/project/mrmetadata/commons_filecount_query.txt > /data/project/mrmetadata/commons_filecount.txt
 /data/project/mrmetadata/bin/python mrmetadata.py --commons
+mysql --defaults-file=/data/project/mrmetadata/.my.cnf -h commonswiki.labsdb < /data/project/mrmetadata/commons_noinfo_users_query.txt > /data/project/mrmetadata/public_html/commons/commons/commons_noinfo_users_results.txt
